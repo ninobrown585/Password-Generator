47a350d8b74a7d9af76583197afbf6192884b61c
Nino Brown <ninobrown58518@gmail.com>
2022-08-18 16:58:47 -0500

    Updated readme and js

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,32 @@
 // Assignment code here
+ var generatePassword = document.getElementById("generate");
 
+ var pwdOptions = {
 
+  // length of password
+  pwdLength: 0,
+
+  //lowercase letters
+  lowerCaseLet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+  //Uppercase Letters
+  upperCaseLet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+
+  //numbers
+  pwdNumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+  //array to hold special characters
+  spcCharacter: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+    "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]//32
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
